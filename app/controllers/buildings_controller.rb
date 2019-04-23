bb1de5b008fcb12550c03d613bc23dc8d1b1db22
Feña Agar <fernando.agar@gmail.com>
2019-04-24 00:26:57 +0200

    Added year distribution and switched route names to be consistent

--- app/controllers/buildings_controller.rb
+++ app/controllers/buildings_controller.rb
@@ -1,12 +1,18 @@
 class BuildingsController < ApplicationController
 
   def index
-    @buildings = Building.limit(100).to_json(except: [:created_at, :updated_at])
-    render json: @buildings
+    response = Building.limit(100).to_json(except: [:created_at, :updated_at])
+    render json: response
   end
 
-  def per_zipcode
-    @buildings = Building.all.group(:plz).count
-    render json: @buildings
+  def zipcode_distribution
+    response = Building.all.group(:plz).count
+    render json: response
+  end
+
+  def year_distribution
+    grouped  = Building.group("date(str_datum)").count.map{|k,v| [Date.parse(k).year, v]}
+    response = Hash[*grouped.flatten].to_json
+    render json: response
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  get '/buildings', to: 'buildings#index'
-  get '/buildings/per_zipcode', to: 'buildings#per_zipcode'
+  get '/buildings',                      to: 'buildings#index'
+  get '/buildings/zipcode_distribution', to: 'buildings#zipcode_distribution'
+  get '/buildings/year_distribution',    to: 'buildings#year_distribution'
 end
